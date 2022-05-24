85faabe2f3965ccf3a97bb75d0638eec551b39cb
陈日雄 <3287956581@qq.com>
2022-05-24 16:20:45 +0800

    简单测试

--- CinemaTicketingSystem/Form1.cs
+++ CinemaTicketingSystem/Form1.cs
@@ -16,5 +16,10 @@ namespace CinemaTicketingSystem
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
